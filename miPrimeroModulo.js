428da41b17c3735f6311d5e73ced8a9afca14c6c
Carola <111399169+MCarolG@users.noreply.github.com>
2024-02-18 17:13:12 -0500

    Commit de Parametros

---
+++ miPrimeroModulo.js
@@ -0,0 +1,8 @@
+exports.sayHello = function(name){
+
+    return "Hello, "+name+", regards!";
+};
+
+exports.suma = function(a, b){
+    return a + b
+}
--- server.js
+++ server.js
@@ -4,9 +4,25 @@
 //console.log(resultado);
 
 const http = require('http');
+const miPrimerModulo = require('./miPrimeroModulo');
+const url = require('url');
 
 http.createServer( function (req, res){
 
-    res.end("Hola Mundo");
+    const saludo = miPrimerModulo.sayHello("Carolina")
+    const resultadoSuma = miPrimerModulo.suma(5,10);
+    const urlInfo = req.url;
+    const q = url.parse(req.url, true).query;
+    const suma = miPrimerModulo.suma(parseFloat(q.num1), parseFloat(q.num2));
+
+    res.write("<h1>"+saludo+"</h1>");
+    res.write(`<p>Esta es la informacion de la URL ${urlInfo} </p>`)
+    res.write(`<p>Resultado de Sumas de Parametros ${suma} </p>`)
+    res.end("Resultado de suma = " +resultadoSuma)
+
+    //res.end("<h1>"+saludo+"</h1>");
+
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.end("<h1>Hola Mundo</h1>");
 
 }).listen(5000);
